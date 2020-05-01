1dea9f883244c8f7e5e3013f816fd926f3e4d0a2
OscarRecinosxd <00128519@uca.edu.sv>
2020-04-30 20:37:32 -0600

    add semana07 exercises

---
+++ Semana07_Ejercicio01/Semana06_Ejercicio01/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Semana06_Ejercicio01;
+
+public class Program {
+    static void Main() {
+        bool continuar = true;
+        do{
+            Console.WriteLine(menu());
+            int opcion = 0;
+            try
+            {
+                opcion = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Dato inválido");
+            }
+            switch(opcion){
+                case 1: Banco.registrarTarjeta(); break;
+                case 2: Banco.consultarTarjetas(); break;
+                case 3: ConsolaJuegos.comprarJuego(); break;
+                case 4: ConsolaJuegos.jugar(); break;
+                case 5: continuar = false; break;
+            }
+        }while(continuar);
+        Console.WriteLine("\nTenga un buen día!");
+    }
+    
+    static string menu(){
+        return "\nBienvenido:\n" +
+               "1) Registrar tarjeta (banco).\n" +
+               "2) Consultar tarjetas (banco).\n" +
+               "3) Comprar videojuego (consola).\n" +
+               "4) Jugar videojuego (consola).\n" +
+               "5) Salir.\n" + 
+               "Opción elegida: \n";
+    }
+}
